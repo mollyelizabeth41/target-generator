85679ec7cf3f7cf6a8cb9127b61599297964dd49
code-mf <marklfranz@gmail.com>
2021-02-14 15:47:06 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
     createCanvas(windowWidth, windowHeight, SVG);
 
     d = select('.div-block');
-    e = select('.div-block-3');
+   // e = select('.div-block-3');
     f = select('#my-gui-container');
     d.position(0, 0);
     rectMode(CENTER);
@@ -17,7 +17,17 @@ function setup() {
 
 function draw() {
     background(gui.bColor);
-    grid();
+    
+        for (let i = windowWidth * gui.margin; i <= windowWidth * (1 - gui.margin); i += windowWidth * gui.xspacing) {
+        for (let y = windowHeight * gui.ymargin; y <= windowHeight * (1 - gui.ymargin); y += windowHeight * gui.yspacing) {
+            if (gui.randomScale) {
+                distance = random(gui.Scale);
+            } else {
+                distance = gui.Scale;
+            }
+            target(i, y, distance, gui.Circles);
+        }
+    }
     noLoop();
 }
 
@@ -28,19 +38,6 @@ function target(xPos, yPos, steps, num) {
     stroke(gui.color);
     for (var i = 0; i <= num; i++) {
         scribble.scribbleEllipse(xPos, yPos, steps * i, steps * i);
-    }
-}
-
-function grid() {
-    for (let i = windowWidth * gui.margin; i <= windowWidth * (1 - gui.margin); i += windowWidth * gui.xspacing) {
-        for (let y = windowHeight * gui.ymargin; y <= windowHeight * (1 - gui.ymargin); y += windowHeight * gui.yspacing) {
-            if (gui.randomScale) {
-                distance = random(gui.Scale);
-            } else {
-                distance = gui.Scale;
-            }
-            target(i, y, distance, gui.Circles);
-        }
     }
 }
 
@@ -65,14 +62,14 @@ function guiSetup() {
     gui_setup.add(gui, 'ymargin', 0, .5).step(.125).onChange(update);
     gui_setup.add(gui, 'xspacing', .125, .25).step(.125).onChange(update);
     gui_setup.add(gui, 'yspacing', .125, .25).step(.125).onChange(update);
-    gui_setup.addColor(gui, 'color').onChange(update);
+    gui_setup.addColor(gui, 'color').onChange(update);;
     gui_setup.addColor(gui, 'bColor').onChange(update);
     //let gui_d = gui_setup.addFolder('Description');
-    gui_setup.add(gui, 'dbWidth', 0, 20).step(1).onChange(description);
+    //gui_setup.add(gui, 'dbWidth', 0, 20).step(1).onChange(description);
     gui_setup.add(gui, 'description').onChange(description);
 
     gui_setup.addColor(gui, 'dColor').onChange(description);
-    gui_setup.close();
+    gui_setup.open();
 
     var obj = {
         Download_SVG: function () {
@@ -93,8 +90,8 @@ function description() {
     if (gui.description) {
 
         d.style('color', gui.dColor);
-        d.style('border', gui.dbWidth + 'px' + ' solid');
-        e.style('border-bottom', gui.dbWidth + 'px' + ' solid');
+        //d.style('border', gui.dbWidth + 'px' + ' solid');
+        //e.style('border-bottom', gui.dbWidth + 'px' + ' solid');
         d.show();
 
     } else {
@@ -103,18 +100,18 @@ function description() {
 }
 
 function Gui() {
-    this.Circles = 5;
-    this.Scale = 37;
-    this.strokeWeight = 1;
-    this.margin = .25;
-    this.ymargin = .25;
-    this.xspacing = .13;
-    this.yspacing = .13;
+    this.Circles = 2;
+    this.Scale = 30;
+    this.strokeWeight = 2;
+    this.margin = .125;
+    this.ymargin = .125;
+    this.xspacing = .125;
+    this.yspacing = .125;
     this.randomScale = true;
-    this.color = [255, 255, 255];
+    this.color = [221, 255, 195];
     this.description = true;
-    this.bColor = '#0077ff';
-    this.dbWidth = 0;
+    this.bColor = '#e49f00';
+    //this.dbWidth = 0;
     this.dColor = '#000000';
 
 }
